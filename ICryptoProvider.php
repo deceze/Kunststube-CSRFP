1366257ba21298042c410d4504e405967c08c106
David Zentgraf <deceze@gmail.com>
2012-11-10 23:39:15 +0100

    Converting tabs to spaces
    *sigh* Editor defaults… -_-;;

--- ICryptoProvider.php
+++ ICryptoProvider.php
@@ -5,7 +5,7 @@ namespace Kunststube\CSRFP;
 
 interface ICryptoProvider {
 
-	public function getRandomHexString($length);
-	public function hash($data, $secret);
+    public function getRandomHexString($length);
+    public function hash($data, $secret);
 
 }
